811a16beaf880981beadf4510d41ad43aa2e99e6
FiifiAsare <don.dc.carlos@gmail.com>
2022-09-19 20:04:14 -0700

    Task 7

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+!#/user/bin/python3
+word = "Holberton"
+word_first_3 = word[0:3]
+word_last_ = word[-2:]
+word_middle = word[1:-1]
+print(f"{word[0:3]}")
+print(f"{word[-2:]}")
+print(f"{word[1:-1]}")
