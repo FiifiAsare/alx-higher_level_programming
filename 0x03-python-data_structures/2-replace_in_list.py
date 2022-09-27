6bec399482080a640408156c9e916f82b12f4f44
FiifiAsare <don.dc.carlos@gmail.com>
2022-09-26 18:46:40 -0700

    Task 3

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 
 def replace_in_list(my_list, idx, element):
-idx != negative
-print("my_list", idx)
-if idx > my_list:
-print("my_list")
+    idx != negative
+        print("my_list", idx)
+    if idx > my_list:
+        print("my_list")
 =======
 def replace_in_list(my_list, idx, element):
     if idx < 0:
@@ -19,4 +18,3 @@ def replace_in_list(my_list, idx, element):
     my_list[idx] = element
 
     return (my_list)
->>>>>>> 601ee016ffb54b13cebb96d7cfcc7853d8f63f21
