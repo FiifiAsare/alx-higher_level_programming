a6b05f5d33a885d78b1d02b7cf78ee702850c183
FiifiAsare <don.dc.carlos@gmail.com>
2022-09-28 19:01:24 -0700

    Task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    my_list = (1, 2, 3, 4, 5, 6)
-    replace = ("Search")
-    print("my_list", replace)
+    def find_search(element):
+        return element if element != search else replace
+    return list(map(find_search, my_list))
